cc2e1a50f990c7cfccb3fc11ff698526805852b1
Wellerson <Wellers0n_@hotmail.com>
2018-07-18 14:42:30 -0300

    first

---
+++ assets/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta charset="UTF-8">
+    <title>Medicenter</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+     <header>
+         <div class="container">
+             <div class="logomarca"><img src="imgs/logo.png" alt="Logo"></div>
+             <div class="menu">
+                 <nav>
+                     <ul>
+                         <li  class="active"><a href="">Home</a></li>
+                         <li><a href="">Sobre</a></li>
+                         <li><a href="">Contatos</a></li>
+                         <li><a href="">Serviços</a></li>
+                     </ul>
+                 </nav>
+             </div>
+         </div>
+     </header>
+     <section id="banner">
+         <div class="container coluna">
+             <div class="banner-text">
+                 <h1>Top notch experience</h1>
+                 <h2>Medicenter is a resposive template perfect for all screen sizes</h2>
+             </div>
+             <div class="banner-options">
+                 <div class="banner1"><div class="banner-title">Emergency Case</div></div>
+                 <div class="banner2"><div class="banner-title">Doctors Timetable</div></div>
+                 <div class="banner3"><div class="banner-title">Opening Hours</div></div>
+             </div>
+         </div>
+     </section>
+</body>
+</html>
